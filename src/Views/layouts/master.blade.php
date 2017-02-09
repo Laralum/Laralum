4e24d8f4619ccb6cd474511fde93fea4db604e5b
Erik Campobadal <soc@erik.cat>
2017-02-09 15:07:21 +0100

    Clicking logo goes to index

--- src/Views/layouts/master.blade.php
+++ src/Views/layouts/master.blade.php
@@ -19,7 +19,7 @@
                 <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <a class="navbar-brand" href="#">{{ config('laralum.general.name') }}</a>
+                <a class="navbar-brand" href="{{ route('laralum::index') }}">{{ config('laralum.general.name') }}</a>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav mr-auto">
                       @foreach(Laralum\Laralum\Packages::all() as $package)
