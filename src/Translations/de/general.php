16edec66778751b43e4dbacb0cfcc2c4677e5405
24aitor <contact@aitorriba.com>
2017-04-14 21:06:44 +0200

    Added Translations

---
+++ src/Translations/de/general.php
@@ -0,0 +1,48 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | General Language Lines
+    |--------------------------------------------------------------------------
+    */
+
+    'language' => 'Sprache',
+    'menu' => 'Menü',
+    'okai' => 'Okai',
+    'success' => 'Erfolg!',
+    'error' => 'Fehler!',
+    'info' => 'Info!',
+    'invalid_form' => 'Ungültiges Formular',
+    'create' => 'Erstellen',
+    'edit' => 'Bearbeiten',
+    'user' => 'Benutzer',
+    'actions' => 'Aktionen',
+    'close' => 'Schließen',
+    'open' => 'Öffnen',
+    'update' => 'Aktualisieren',
+    'whops' => 'Whops',
+    'nice' => 'Nett',
+    'cancel' => 'Stornieren',
+    'watch_out' => 'Achtung',
+    'name' => 'Name',
+    'email' => 'Email',
+    'logout' => 'Ausloggen',
+    'register' => 'Neu registrieren',
+    'password' => 'Passwort',
+    'password_confirmation' => 'Passwort Bestätigung',
+    'current_password' => 'Aktuelles Passwort',
+    'remember_me' => 'Erinnere dich an mich',
+    'submit' => 'Einreichen',
+    'loading' => 'Laden',
+    'online' => 'Online',
+    'away' => 'Ein Weg',
+    'proceed' => 'Vorgehen',
+    'perform_action' => 'Sind Sie sicher, dass Sie diese Aktion durchführen möchten?',
+    'confirmation_page' => 'Bestätigungsseite',
+    'take_me_back' => 'Nimm mich zurück',
+    'confirmation_proceed' => 'Bist du sicher, dass du fortfahren willst?',
+    'confirmation_info' => 'Diese Aktion kann nicht rückgängig gemacht werden.',
+    'home' => 'Zuhause',
+];
---
+++ src/Translations/fr/general.php
@@ -0,0 +1,48 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | General Language Lines
+    |--------------------------------------------------------------------------
+    */
+
+    'language' => 'La langue',
+    'menu' => 'Menu',
+    'okai' => 'Okai',
+    'success' => 'Succès!',
+    'error' => 'Erreur!',
+    'info' => 'Info!',
+    'invalid_form' => 'Forme non valide',
+    'create' => 'Créer',
+    'edit' => 'Modifier',
+    'user' => 'Utilisateur',
+    'actions' => 'Actes',
+    'close' => 'Fermer',
+    'open' => 'Ouvrir',
+    'update' => 'Mettre à jour',
+    'whops' => 'Whops',
+    'nice' => 'Agréable',
+    'cancel' => 'Annuler',
+    'watch_out' => 'Fais attention',
+    'name' => 'Prénom',
+    'email' => 'Email',
+    'logout' => 'Connectez - Out',
+    'register' => 'Registre',
+    'password' => 'Mot de passe',
+    'password_confirmation' => 'Confirmation mot de passe',
+    'current_password' => 'Mot de passe actuel',
+    'remember_me' => 'Souviens-toi de moi',
+    'submit' => 'Soumettre',
+    'loading' => 'Chargement',
+    'online' => 'En ligne',
+    'away' => 'Un moyen',
+    'proceed' => 'Procéder',
+    'perform_action' => 'Êtes-vous sûr de vouloir effectuer cette action?',
+    'confirmation_page' => 'Page de confirmation',
+    'take_me_back' => 'Reprends moi',
+    'confirmation_proceed' => 'Etes-vous sûr de vouloir continuer?',
+    'confirmation_info' => 'Cette action ne peut pas être annulée.',
+    'home' => 'Accueil',
+];
