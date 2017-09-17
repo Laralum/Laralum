8101aabf4c4bbc6d48c00f0bf01a194c6c0b2b23
Aitor Riba Archilla <contact@aitorriba.com>
2017-09-17 17:02:02 +0200

    Recoded pagination layout and fixed composer.json

--- src/Views/layouts/pagination.blade.php
+++ src/Views/layouts/pagination.blade.php
@@ -1,23 +1,36 @@
-@if ($paginator->lastPage() > 1)
-<ul class="uk-pagination uk-flex-center uk-margin-medium-top" uk-margin>
-    <li class="{{ !($paginator->currentPage() == 1) ?: 'uk-disabled' }}">
-        <a href="{{ $paginator->url(1) }}">
-            <span uk-pagination-previous></span>
-        </a>
-    </li>
-    <li class="uk-disabled {{ !($paginator->currentPage() - 5 <= 1 ) ?: 'uk-hidden' }}"><span>...</span></li>
-    @php
-        $minPag = ($paginator->currentPage() - 5 <= 1 ) ? 1 : $paginator->currentPage() - 5;
-        $maxPag = ($paginator->currentPage() + 5 >= $paginator->lastPage()) ? $paginator->lastPage() : $paginator->currentPage() + 5;
-    @endphp
-    @for ($i = $minPag; $i <= $maxPag; $i++)
-        <li class="{{ !($paginator->currentPage() == $i) ?: 'uk-active' }}">
-            <a href="{{ $paginator->url($i) }}">{{ $i }}</a>
-        </li>
-    @endfor
-    <li class="uk-disabled {{ !($paginator->currentPage() + 5 >= $paginator->lastPage()) ?: 'uk-hidden' }}"><span>...</span></li>
-    <li class="{{ !($paginator->currentPage() == $paginator->lastPage()) ?: 'uk-disabled' }}">
-        <a href="{{ $paginator->url($paginator->currentPage()+1) }}"><span uk-pagination-next></span></a>
-    </li>
-</ul>
+@if ($paginator->hasPages())
+    <ul class="uk-pagination uk-flex-center" uk-margin>
+        {{-- Previous Page Link --}}
+        @if ($paginator->onFirstPage())
+            <li class="uk-disabled"><a href="#"><span uk-pagination-previous></span></a></li>
+        @else
+            <li><a href="{{ $paginator->previousPageUrl() }}"><span uk-pagination-previous></span></a></li>
+        @endif
+
+        {{-- Pagination Elements --}}
+        @foreach ($elements as $element)
+            {{-- "Three Dots" Separator --}}
+            @if (is_string($element))
+                <li class="uk-disabled"><span>...</span></li>
+            @endif
+
+            {{-- Array Of Links --}}
+            @if (is_array($element))
+                @foreach ($element as $page => $url)
+                    @if ($page == $paginator->currentPage())
+                        <li class="uk-active"><span>{{ $page }}</span></li>
+                    @else
+                        <li><a href="{{ $url }}">{{ $page }}</a></li>
+                    @endif
+                @endforeach
+            @endif
+        @endforeach
+
+        {{-- Next Page Link --}}
+        @if ($paginator->hasMorePages())
+            <li><a href="{{ $paginator->nextPageUrl() }}"><span uk-pagination-next></span></a></li>
+        @else
+            <li class="uk-disabled"><a href="#"><span uk-pagination-next></span></a></li>
+        @endif
+    </ul>
 @endif
